f784f79e1c88ca8af9d75a938b22439391665505
Mihailo Petrovic <mihailothemix@gmail.com>
2024-03-30 02:29:45 +0100

    Dodata specijalizacija zahteva i izmenjen gitignore fajl zbog reacta

--- Aplikacija/backend/obj/Debug/net8.0/WebTemplate.AssemblyInfo.cs
+++ Aplikacija/backend/obj/Debug/net8.0/WebTemplate.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebTemplate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d240bec693303ac76307ec20f60c231d0349dde7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebTemplate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebTemplate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
