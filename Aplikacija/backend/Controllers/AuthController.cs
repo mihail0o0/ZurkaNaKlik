43eae0fcb393ba33fa148689fc1e8cf9666120b2
Dusan Pavlovic <dusan.pavlovic016@gmail.com>
2024-05-10 13:21:02 +0200

    izmene oko isteka tokena

--- Aplikacija/backend/Controllers/AuthController.cs
+++ Aplikacija/backend/Controllers/AuthController.cs
@@ -257,7 +257,7 @@ namespace backend.Controllers
 
             var token = new JwtSecurityToken(
                     claims: claims,
-                    expires: DateTime.Now.AddSeconds(30),
+                    expires: DateTime.Now.AddDays(1),
                     signingCredentials: creds
                 );
 
