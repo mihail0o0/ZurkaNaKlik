9ccb3cb0ba9ae180e1b4c987c81198f5f91b7528
teckooo <teodorabozic@elfak.rs>
2024-05-14 20:53:43 +0200

    nestoo

--- Aplikacija/backend/obj/Debug/net8.0/WebTemplate.AssemblyInfo.cs
+++ Aplikacija/backend/obj/Debug/net8.0/WebTemplate.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebTemplate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27f25a4f7386d43057fdc6c425479c8a8e63614e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f62449154c6b8532c58aa211299f8677ff91747")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebTemplate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebTemplate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
