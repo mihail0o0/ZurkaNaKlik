63ac7d6b746161a9d8bc2aef2b1b57d1aec9ecc5
Jana <jana.dilberovic@elfak.rs>
2024-05-24 12:39:23 +0200

    homepage i homeimage element

--- Aplikacija/frontend/src/pages/search/index.tsx
+++ Aplikacija/frontend/src/pages/search/index.tsx
@@ -1,55 +1,17 @@
-import MojButton from "@/components/lib/button";
+
 import style from "./style.module.css";
-import DivFilteri from "./DivFilteri";
+
 import OglasKartica from "@/components/OglasKartica";
-import { useState } from "react";
+
+import HomeImage from "@/components/HomeImage";
 const Search = () => {
-  const nizDugmad = [
-    "Sve",
-    "Rođendan",
-    "Žurka",
-    "Team building",
-    "Momačko veče",
-    "Devojačko vece",
-    "Ostalo",
-  ];
+
   
 
   
   return (
     <div>
-      <div className={style.SearchDiv}>
-        <div className={style.GoreDiv}>
-          <div className={style.TekstDivVeliki}>
-            <h1>Pronađite svoj savršeni prostor!</h1>
-          </div>
-          <div className={style.TekstDivMali}>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-              mattis ornare tortor sed dignissim. Nunc ac ipsum placerat, cursus
-              arcu in, facilisis purus.{" "}
-            </p>
-          </div>
-        </div>
-        <div className={style.DoleDiv}>
-          <div className={style.Dugmad}>
-            {nizDugmad.map((dugme, index) => {
-              return (
-                <MojButton
-                  text={dugme}
-                  onClick={() => {}}
-                  paddingX="12px"
-                  paddingY="15px"
-                  fontSize="15px"
-                  backgroundColor="white"
-                  color="black"
-                />
-              );
-            })}
-          </div>
-          <DivFilteri />
-        </div>
-      </div>
+      <HomeImage />
       <div className={style.SearchKartice}>
         <OglasKartica
           nazivProstora="Vila Maria"
