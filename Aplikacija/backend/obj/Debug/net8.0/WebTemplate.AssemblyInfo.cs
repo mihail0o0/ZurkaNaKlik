316ef6533b2ac8c50d8ff8122abc118ce473a59e
mihailooo <mihailothemix@gmail.com>
2024-05-26 13:29:00 +0200

    Merge changes

--- Aplikacija/backend/obj/Debug/net8.0/WebTemplate.AssemblyInfo.cs
+++ Aplikacija/backend/obj/Debug/net8.0/WebTemplate.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebTemplate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c48dc3311dade65cab09f1142a82333980d8a5dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63ac7d6b746161a9d8bc2aef2b1b57d1aec9ecc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebTemplate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebTemplate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
