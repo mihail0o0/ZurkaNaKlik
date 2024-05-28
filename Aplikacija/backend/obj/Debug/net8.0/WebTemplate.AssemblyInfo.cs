3cfa4fcb4b31f4f40580eda26b2620d1b141012e
Jana <jana.dilberovic@elfak.rs>
2024-05-28 18:20:02 +0200

    merged

--- Aplikacija/backend/obj/Debug/net8.0/WebTemplate.AssemblyInfo.cs
+++ Aplikacija/backend/obj/Debug/net8.0/WebTemplate.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebTemplate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20c3e684a6c9652acbee72a634eb6c2733ca304c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebTemplate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebTemplate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
