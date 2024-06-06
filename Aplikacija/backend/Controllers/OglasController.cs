1e889934bb8a4e302db73ee9604464d19a2a6e35
mihailooo <mihailothemix@gmail.com>
2024-06-06 18:55:59 +0200

    vratioglase izmenjena

--- Aplikacija/backend/Controllers/OglasController.cs
+++ Aplikacija/backend/Controllers/OglasController.cs
@@ -48,11 +48,12 @@ namespace backend.Controllers
         #endregion
 
         #region VratiOglaseSaFilterimaISortiranjem
-        [HttpPost("VratiOglase/{pageNumber}/{pageSize}")]
-        public async Task<ActionResult> VratiOglase([FromBody] Filters filteri, int pageNumber, int pageSize)
-        { //dodaj sortiranje
+        [HttpPost("VratiOglase/{pageNumber}/{pageSize}/{sort}")]
+        public async Task<ActionResult> VratiOglase([FromBody] Filters filteri, int pageNumber, int pageSize, string sort)
+        {
             try
             {
+
                 // public class Filters
                 // {
                 //     public List<EnumTipProslava>? TipProslava { get; set; }
@@ -67,9 +68,10 @@ namespace backend.Controllers
                 //     public DateTime DatumOd { get; set; }
                 //     public DateTime DatumDo { get; set; }
                 // }
+
                 List<OglasObjekta> oglasi = await Context.OglasiObjekta.ToListAsync();
 
-                switch (filteri.sort)
+                switch (sort)
                 {
                     case "CenaRastuca":
                         oglasi = oglasi.OrderBy(o => o.CenaPoDanu).ToList();
@@ -78,10 +80,10 @@ namespace backend.Controllers
                         oglasi = oglasi.OrderByDescending(o => o.CenaPoDanu).ToList();
                         break;
                     case "OcenaRastuce":
-                        oglasi = oglasi.OrderBy(o => ((double)(o.Ocena ?? 0))).ToList();
+                        oglasi = oglasi.OrderBy(o => (double)(o.Ocena ?? 0)).ToList();
                         break;
                     case "OcenaOpadajuce":
-                        oglasi = oglasi.OrderByDescending(o => ((double)(o.Ocena ?? 0))).ToList();
+                        oglasi = oglasi.OrderByDescending(o => (double)(o.Ocena ?? 0)).ToList();
                         break;
                     default:
                         return BadRequest("Ne postoji sort");
@@ -102,14 +104,25 @@ namespace backend.Controllers
                     oglasi = oglasi.Where(oglas => oglas.Grad.Equals(filteri.grad)).ToList();
                 }
 
-                if (filteri.cenaOd >= 0 && filteri.cenaDo <= Int32.MaxValue && filteri.cenaOd < filteri.cenaDo)
+
+                if (filteri.cenaOd != null && filteri.cenaOd >= 0 && (filteri.cenaDo == null || filteri.cenaOd < filteri.cenaDo))
                 {
-                    oglasi = oglasi.Where(oglas => oglas.CenaPoDanu >= filteri.cenaOd && oglas.CenaPoDanu <= filteri.cenaDo).ToList();
+                    oglasi = oglasi.Where(oglas => oglas.CenaPoDanu >= filteri.cenaOd).ToList();
                 }
 
-                if (filteri.kvadraturaOd >= 0 && filteri.kvadraturaDo <= Int32.MaxValue && filteri.kvadraturaOd < filteri.kvadraturaDo)
+                if (filteri.cenaDo != null && filteri.cenaDo <= Int32.MaxValue && (filteri.cenaOd == null || filteri.cenaOd < filteri.cenaDo))
                 {
-                    oglasi = oglasi.Where(oglas => oglas.Kvadratura >= filteri.kvadraturaOd && oglas.Kvadratura <= filteri.kvadraturaDo).ToList();
+                    oglasi = oglasi.Where(oglas => oglas.CenaPoDanu <= filteri.cenaDo).ToList();
+                }
+
+                if (filteri.kvadraturaOd != null && filteri.kvadraturaOd >= 0 && (filteri.kvadraturaDo == null || filteri.kvadraturaOd < filteri.kvadraturaDo))
+                {
+                    oglasi = oglasi.Where(oglas => oglas.Kvadratura >= filteri.kvadraturaOd).ToList();
+                }
+
+                if (filteri.kvadraturaDo != null && filteri.kvadraturaDo <= Int32.MaxValue && (filteri.kvadraturaOd == null || filteri.kvadraturaOd < filteri.kvadraturaDo))
+                {
+                    oglasi = oglasi.Where(oglas => oglas.Kvadratura <= filteri.kvadraturaDo).ToList();
                 }
 
                 if (filteri.grejanje != null)
@@ -122,8 +135,10 @@ namespace backend.Controllers
                     oglasi = oglasi.Where(oglas => oglas.ListDodatneOpreme.Any(tip => filteri.dodatnaOprema!.Contains(tip))).ToList();
                 }
 
-                List<DateTime> sviDaniUOpsegu = Enumerable.Range(0, (filteri.datumDo.Date - filteri.datumOd.Date).Days + 1)
-                                            .Select(offset => filteri.datumOd.AddDays(offset))
+                DateTime datumOd = filteri.datumOd ?? DateTime.MinValue;
+                DateTime datumDo = filteri.datumDo ?? DateTime.MaxValue;
+                List<DateTime> sviDaniUOpsegu = Enumerable.Range(0, (datumOd - datumDo).Days + 1)
+                                            .Select(offset => datumOd.AddDays(offset))
                                             .ToList();
 
                 oglasi = oglasi.Where(oglas => !oglas.ZauzetiDani!.Any(zauzetDan => sviDaniUOpsegu.Contains(zauzetDan)))
@@ -147,6 +162,108 @@ namespace backend.Controllers
             }
         }
         #endregion
+
+
+        // #region VratiOglaseSaFilterimaISortiranjem
+        // [HttpPost("VratiOglase/{pageNumber}/{pageSize}")]
+        // public async Task<ActionResult> VratiOglase([FromBody] Filters filteri, int pageNumber, int pageSize)
+        // { //dodaj sortiranje
+        //     try
+        //     {
+        //         // public class Filters
+        //         // {
+        //         //     public List<EnumTipProslava>? TipProslava { get; set; }
+        //         //     public List<EnumTipProstora>? TipProstora { get; set; }
+        //         //     public string? Grad { get; set; }
+        //         //     public int CenaOd { get; set; }
+        //         //     public int CenaDo { get; set; }
+        //         //     public int KvadraturaOd { get; set; }
+        //         //     public int KvadraturaDo { get; set; }
+        //         //     public List<EnumGrejanje>? Grejanje { get; set; }
+        //         //     public List<EnumDodatnaOprema>? DodatnaOprema { get; set; }
+        //         //     public DateTime DatumOd { get; set; }
+        //         //     public DateTime DatumDo { get; set; }
+        //         // }
+        //         List<OglasObjekta> oglasi = await Context.OglasiObjekta.ToListAsync();
+
+        //         switch (filteri.sort)
+        //         {
+        //             case "CenaRastuca":
+        //                 oglasi = oglasi.OrderBy(o => o.CenaPoDanu).ToList();
+        //                 break;
+        //             case "CenaOpadajuce":
+        //                 oglasi = oglasi.OrderByDescending(o => o.CenaPoDanu).ToList();
+        //                 break;
+        //             case "OcenaRastuce":
+        //                 oglasi = oglasi.OrderBy(o => ((double)(o.Ocena ?? 0))).ToList();
+        //                 break;
+        //             case "OcenaOpadajuce":
+        //                 oglasi = oglasi.OrderByDescending(o => ((double)(o.Ocena ?? 0))).ToList();
+        //                 break;
+        //             default:
+        //                 return BadRequest("Ne postoji sort");
+        //         }
+
+        //         if (filteri.tipProslava != null && filteri.tipProslava!.Count != 0)
+        //         {
+        //             oglasi = oglasi.Where(oglas => oglas.ListaTipProslava.Any(tip => filteri.tipProslava!.Contains(tip))).ToList();
+        //         }
+
+        //         if (filteri.tipProstora != null && filteri.tipProstora!.Count != 0)
+        //         {
+        //             oglasi = oglasi.Where(oglas => oglas.ListaTipProstora.Any(tip => filteri.tipProstora!.Contains(tip))).ToList();
+        //         }
+
+        //         if (filteri.grad != null)
+        //         {
+        //             oglasi = oglasi.Where(oglas => oglas.Grad.Equals(filteri.grad)).ToList();
+        //         }
+
+        //         if (filteri.cenaOd >= 0 && filteri.cenaDo <= Int32.MaxValue && filteri.cenaOd < filteri.cenaDo)
+        //         {
+        //             oglasi = oglasi.Where(oglas => oglas.CenaPoDanu >= filteri.cenaOd && oglas.CenaPoDanu <= filteri.cenaDo).ToList();
+        //         }
+
+        //         if (filteri.kvadraturaOd >= 0 && filteri.kvadraturaDo <= Int32.MaxValue && filteri.kvadraturaOd < filteri.kvadraturaDo)
+        //         {
+        //             oglasi = oglasi.Where(oglas => oglas.Kvadratura >= filteri.kvadraturaOd && oglas.Kvadratura <= filteri.kvadraturaDo).ToList();
+        //         }
+
+        //         if (filteri.grejanje != null)
+        //         {
+        //             oglasi = oglasi.Where(oglas => filteri.grejanje.Contains(oglas.Grejanje)).ToList();
+        //         }
+
+        //         if (filteri.dodatnaOprema != null && filteri.dodatnaOprema!.Count != 0)
+        //         {
+        //             oglasi = oglasi.Where(oglas => oglas.ListDodatneOpreme.Any(tip => filteri.dodatnaOprema!.Contains(tip))).ToList();
+        //         }
+
+        //         List<DateTime> sviDaniUOpsegu = Enumerable.Range(0, (filteri.datumDo.Date - filteri.datumOd.Date).Days + 1)
+        //                                     .Select(offset => filteri.datumOd.AddDays(offset))
+        //                                     .ToList();
+
+        //         oglasi = oglasi.Where(oglas => !oglas.ZauzetiDani!.Any(zauzetDan => sviDaniUOpsegu.Contains(zauzetDan)))
+        //                     .ToList();
+
+        //         oglasi = oglasi.Skip((pageNumber - 1) * pageSize)
+        //                      .Take(pageSize).ToList();
+
+        //         List<OglasObjektaResponse> response = new List<OglasObjektaResponse>();
+
+        //         foreach (OglasObjekta oglas in oglasi)
+        //         {
+        //             response.Add(ObjectCreatorSingleton.Instance.ToOglasResult(oglas));
+        //         }
+
+        //         return Ok(response);
+        //     }
+        //     catch (Exception e)
+        //     {
+        //         return BadRequest(e);
+        //     }
+        // }
+        // #endregion
 
         #region VratiSveGradove
         [HttpGet("VratiSveGradove")]
@@ -220,9 +337,9 @@ namespace backend.Controllers
             try
             {
 
-                var vlasnik = await Context.OglasiObjekta.Where(x =>x.Id == idoglasa).Select(x => 
+                var vlasnik = await Context.OglasiObjekta.Where(x => x.Id == idoglasa).Select(x =>
                     x.VlasnikOglasa).FirstOrDefaultAsync();
-            
+
 
                 if (vlasnik == null)
                 {
--- Aplikacija/backend/DTOs/Filters.cs
+++ Aplikacija/backend/DTOs/Filters.cs
@@ -7,17 +7,16 @@ namespace backend.DTOs
 {
     public class Filters
     {
-        public string? sort { get; set; }
         public List<EnumTipProslava>? tipProslava { get; set; }
         public List<EnumTipProstora>? tipProstora { get; set; }
         public string? grad { get; set; }
-        public int cenaOd { get; set; }
-        public int cenaDo { get; set; }
-        public int kvadraturaOd { get; set; }
-        public int kvadraturaDo { get; set; }
+        public int? cenaOd { get; set; }
+        public int? cenaDo { get; set; }
+        public int? kvadraturaOd { get; set; }
+        public int? kvadraturaDo { get; set; }
         public List<EnumGrejanje>? grejanje { get; set; }
         public List<EnumDodatnaOprema>? dodatnaOprema { get; set; }
-        public DateTime datumOd { get; set; }
-        public DateTime datumDo { get; set; }
+        public DateTime? datumOd { get; set; }
+        public DateTime? datumDo { get; set; }
     }
 }
