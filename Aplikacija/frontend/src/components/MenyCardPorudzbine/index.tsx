f6c6fda7ffe64fa1ffd0dbdc7afffee68868434e
Jana <jana.dilberovic@elfak.rs>
2024-06-07 14:33:48 +0200

    porudzbine sljaka

--- Aplikacija/frontend/src/components/MenyCardPorudzbine/index.tsx
+++ Aplikacija/frontend/src/components/MenyCardPorudzbine/index.tsx
@@ -3,7 +3,11 @@ import style from "./style.module.css";
 import DisplayCard from "../DisplayCard";
 import MojButton from "../lib/button";
 import Icon from "../lib/icon";
-import { useGetMenuesQuery } from "@/store/api/endpoints/agencija";
+import { useAcceptOrderMutation, useDeclineOrderMutation, useGetMenuesQuery } from "@/store/api/endpoints/agencija";
+import { useGetOglasQuery } from "@/store/api/endpoints/oglas";
+import { skipToken } from "@reduxjs/toolkit/query";
+import { toast } from "react-toastify";
+import { useEffect, useState } from "react";
 
 type MenyCardProps = {
   order?: CateringOrder;
@@ -11,6 +15,54 @@ type MenyCardProps = {
   //meny
 };
 const MenyCardPorudzbine = ({ order, meni }: MenyCardProps) => {
+  const{data :oglas}=useGetOglasQuery(order?.idOglasa ?? skipToken);
+  const[acceptOrder]=useAcceptOrderMutation();
+  const[declineOrder]=useDeclineOrderMutation();
+  const [statusRezervacije, setStatusRezervacije] = useState(order?.statusRezervacije);
+
+  const formatDate = (date:Date) => {
+    if (!(date instanceof Date)) return 'Invalid date';
+    
+    const day = date.getDate();
+    const month = date.getMonth() + 1; 
+    const year = date.getFullYear();
+
+    return `${day}.${month}.${year}.`;
+  };
+  const accept = async () => {
+    if(!order) {
+      console.log("nema order");
+      return;
+    }
+   const result = await acceptOrder(order.id);
+
+    if ("error" in result) {
+      return;
+    }
+    setStatusRezervacije(true);
+    toast.success("Porudzbina je prihvacena!");
+    
+  };
+  const decline = async () => {
+    if(!order) {
+      console.log("nema order");
+      return;
+    }
+   const result = await declineOrder(order.id);
+
+    if ("error" in result) {
+      return;
+    }
+    setStatusRezervacije(false);
+    toast.success("Porudzbina je odbijena!");
+    
+  };
+  useEffect(() => {
+    if (order) {
+      setStatusRezervacije(order.statusRezervacije);
+    }
+  }, [order]);
+ console.log(order);
   return (
     <div className={`containerWrapper ${style.Container}`}>
       <div className={style.MenyPicture}>
@@ -18,40 +70,44 @@ const MenyCardPorudzbine = ({ order, meni }: MenyCardProps) => {
       </div>
       <div className={style.MenyTxt}>
         <h3>{meni && meni.naziv}</h3>
-        {/* <p>Opis menija neki mali.</p> */}
+        
+        {/* <p>Opis menija neki mali./p> */}
         <div className={style.InfoOglas}>
           <div className={style.DisplayMenyInfo}>
             <>
               <Icon icon={"location_on"} />
-              <label>{"Trg 14. oktobra 6"}</label>
+              <label>{oglas && oglas.lokacija}</label>
               
             </>
           </div>
           <div className={style.DisplayMenyInfo}>
             <>
               <Icon icon={"calendar_month"} />
-              <label>{"14.5.2024."}</label>{" "}
+              <label>{order?.datumRezervacije && order.datumRezervacije}</label>
             </>
           </div>
         </div>
         <div className={style.InfoOglas}>
           <div className={style.DisplayMenyInfo}>
             <Icon icon={"house"} />
-            <label>{"Vila ramonda"}</label>
+            <label>{oglas && oglas.naziv}</label>
           </div>
           <div className={style.DisplayMenyInfo}>
             <Icon icon={"payments"} />
-            <label>{"200"}</label>{" "}
+            <label>{order?.konacnaCena}</label>{" "}
           </div>
         </div>
       </div>
-      <div className={style.acceptDecline}>
+     {!order?.statusRezervacije &&
+     
+     <div className={style.acceptDecline}>
         <div className={style.ButtonParentDiv}>
           <MojButton
             icon="check"
             text="Prihvati"
             backgroundColor="#00D615"
             wide={true}
+            onClick={accept}
           />
         </div>
         <div className={style.ButtonParentDiv}>
@@ -60,9 +116,10 @@ const MenyCardPorudzbine = ({ order, meni }: MenyCardProps) => {
             text="Odbij"
             backgroundColor="#FA3636"
             wide={true}
+            onClick={decline}
           />
         </div>
-      </div>
+      </div>}
     </div>
   );
 };
--- Aplikacija/frontend/src/pages/Porudzbine/index.tsx
+++ Aplikacija/frontend/src/pages/Porudzbine/index.tsx
@@ -1,26 +1,67 @@
 import { useGetCateringOrdersQuery } from "@/store/api/endpoints/agencija";
 import style from "./style.module.css";
 import { useGetAgencyListMenuesQuery } from "@/store/api/endpoints/pregled";
+import MenyCardPorudzbine from "@/components/MenyCardPorudzbine";
 
-const Porudzbine = () =>{
-    const {data: narudzbine}=useGetCateringOrdersQuery();
-     
-    const odobreneNarudzbine = narudzbine ? narudzbine.filter((narudzbina) => narudzbina.statusRezervacije === true) : [];
-    const odbijeneNarudzbine = narudzbine ? narudzbine.filter((narudzbina) => narudzbina.statusRezervacije === false) : [];
-    const narudzbineNaCekanju = narudzbine ? narudzbine.filter((narudzbina) => narudzbina.statusRezervacije === null) : [];
-  
-    const menijiOdobreniID = odobreneNarudzbine.flatMap(order => order.idMenija || []);
-    const menijiNaCekanjuID=narudzbineNaCekanju.flatMap(order=> order.idMenija || []);
-    const menijiOdbijeniID=odbijeneNarudzbine.flatMap(order=> order.idMenija || []);
+const Porudzbine = () => {
+  const { data: narudzbine } = useGetCateringOrdersQuery();
+  console.log(narudzbine + "naruzbine");
 
-    const{data: menuOdobreni}=useGetAgencyListMenuesQuery(menijiOdobreniID);
-    const{data: menuNaCekanju}=useGetAgencyListMenuesQuery(menijiNaCekanjuID);
-    const{data: menuOdbijeni}=useGetAgencyListMenuesQuery(menijiOdbijeniID);
-    
-    return(
-          <div className={`containerWrapper ${style.Container}`}>
-
-          </div>
-    )
-}
+  const odobreneNarudzbine = narudzbine
+    ? narudzbine.filter((narudzbina) => narudzbina.statusRezervacije === true)
+    : [];
+  const odbijeneNarudzbine = narudzbine
+    ? narudzbine.filter((narudzbina) => narudzbina.statusRezervacije === false)
+    : [];
+  const narudzbineNaCekanju = narudzbine
+    ? narudzbine.filter((narudzbina) => narudzbina.statusRezervacije === null)
+    : [];
+  console.log(narudzbineNaCekanju + "odobrene naruzbine");
+  const menijiOdobreniID = odobreneNarudzbine.flatMap(
+    (order) => order.idMenija || []
+  );
+  const menijiNaCekanjuID = narudzbineNaCekanju.flatMap(
+    (order) => order.idMenija || []
+  );
+  const menijiOdbijeniID = odbijeneNarudzbine.flatMap(
+    (order) => order.idMenija || []
+  );
+  console.log(menijiOdobreniID + "id");
+  // console.log(menijiNaCekanjuID + "id meni");
+  //const { data: menuOdobreni } = useGetAgencyListMenuesQuery(menijiOdobreniID);
+  // console.log(menuOdobreni + "" + "odobreni meniji lista");
+  // const { data: menuNaCekanju } =
+  //   useGetAgencyListMenuesQuery(menijiNaCekanjuID);
+  // const { data: menuOdbijeni } = useGetAgencyListMenuesQuery(menijiOdbijeniID);
+  // console.log(menuNaCekanju + "menijiodobreni");
+  return (
+    <div className={`containerWrapper ${style.Container}`}>
+      <div className={style.OdobrenePorudzbine}>
+        <h3>Nove porudzbine</h3>
+        <div className={style.listPorudzbine}>
+          {narudzbineNaCekanju.map((narudzbina) => {
+            return <MenyCardPorudzbine order={narudzbina} />;
+          })}
+        </div>
+        
+      </div>
+      <div className={style.OdobrenePorudzbine}>
+      <h3>Odobrene porudzbine</h3>
+        <div className={style.listPorudzbine}>
+          {odobreneNarudzbine.map((narudzbina) => {
+            return <MenyCardPorudzbine order={narudzbina} />;
+          })}
+        </div>
+      </div>
+      <div className={style.OdobrenePorudzbine}>
+      <h3>Odbijene porudzbine</h3>
+        <div className={style.listPorudzbine}>
+          {odbijeneNarudzbine.map((narudzbina) => {
+            return <MenyCardPorudzbine order={narudzbina} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Porudzbine;
